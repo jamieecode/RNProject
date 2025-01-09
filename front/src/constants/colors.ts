c4fc04ea86f34f1ae00f8ebfa543f04a5363150e
jamieecode <jamieecode@gmail.com>
2025-01-09 21:36:47 +0900

    feat: add inputField component and useForm hook

--- front/src/constants/colors.ts
+++ front/src/constants/colors.ts
@@ -1,5 +1,11 @@
 export const colors = {
   WHITE: '#FFF',
+  BLACK: '#000',
   PINK_500: '#BF5C79',
   PINK_700: '#C63B64',
+  RED_300: '#FFB4B4',
+  RED_500: '#FF5F5F',
+  GRAY_200: '#E7E7E7',
+  GRAY_500: '#828282',
+  GRAY_700: '#575757',
 };
--- front/src/screens/auth/LoginScreen.tsx
+++ front/src/screens/auth/LoginScreen.tsx
@@ -1,14 +1,61 @@
 import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
-
+import {View, StyleSheet} from 'react-native';
+import InputField from '../../components/InputField';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import CustomButton from '../../components/CustomButton';
+import useForm from '../../hooks/useForm';
+import {validateLogin} from '../../utils/validate';
+validateLogin;
 function LoginScreen() {
+  const login = useForm({
+    initialValue: {
+      email: '',
+      password: '',
+    },
+    validate: validateLogin,
+  });
+
+  const handleSubmit = () => {
+    console.log(login.values);
+  };
+
   return (
-    <View>
-      <Text>로그인스크린</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.inputContainer}>
+        <InputField
+          placeholder="이메일"
+          error={login.errors.email}
+          inputMode="email"
+          touched={login.touched.email}
+          {...login.getTextInputProps('email')}
+        />
+        <InputField
+          placeholder="비밀번호"
+          error={login.errors.password}
+          secureTextEntry
+          touched={login.touched.password}
+          {...login.getTextInputProps('password')}
+        />
+      </View>
+      <CustomButton
+        label="로그인"
+        variant="filled"
+        size="large"
+        onPress={handleSubmit}
+      />
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 30,
+  },
+  inputContainer: {
+    gap: 20,
+    marginBottom: 30,
+  },
+});
 
 export default LoginScreen;
